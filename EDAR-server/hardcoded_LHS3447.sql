c406353844c52c916370350d90ce1470af348cf2
JelleV <3942301+jcjveraa@users.noreply.github.com>
2022-06-23 16:06:58 +0200

    minor changes...

---
+++ EDAR-server/hardcoded_LHS3447.sql
@@ -0,0 +1,57 @@
+select table1.commodity_id, table1.station_id as buy_station_id, 
+buy_station_full.name as buy_station_name,
+table2.station_id as sell_station_id, 
+(buy_system_full.X-sell_system_full.X)*(buy_system_full.X-sell_system_full.X)
+ + (buy_system_full.Y-sell_system_full.Y)*(buy_system_full.Y-sell_system_full.Y) 
+ + (buy_system_full.Z-sell_system_full.Z)*(buy_system_full.Z-sell_system_full.Z)  as jump_distance_LY_Squared,
+sell_station_full.name as sell_station_name,
+table1.supply, 
+	min(table1.buy_price) as buy_price, 
+	table2.sell_price as sell_price,
+	(max(table2.sell_price)-min(table1.buy_price)) as profit_per_unit, 
+	buy_station_full.system_id as buy_system_id, buy_system_full.name as buy_system_name, sell_station_full.system_id  as sell_system_id, sell_system_full.name as sell_system_name
+from listings_v6 as table1
+
+INNER join
+
+		(select *, max(sell_price) from listings_v6 where
+		listings_v6.station_id in
+			(select stations_v6.id as station_id
+			from
+			(SELECT sp1.id, sp1.edsm_id, sp1.name,
+			(sp1.X-sp2.X)*(sp1.X-sp2.X) + (sp1.Y-sp2.Y)*(sp1.Y-sp2.Y) + (sp1.Z-sp2.Z)*(sp1.Z-sp2.Z) AS distance_squared 
+			FROM systems_populated_v6 AS sp1
+			LEFT JOIN systems_populated_v6 AS sp2
+			WHERE sp2.id = 12024
+      AND distance_squared <= 100
+
+			) as systems_in_range
+			LEFT JOIN stations_v6 ON systems_in_range.id = stations_v6.system_id
+      WHERE stations_v6.max_landing_pad_size in ('L')
+      )
+		and demand > 0
+		and collected_at > 3*24*3600
+		group by listings_v6.commodity_id) as table2
+
+on table1.commodity_id = table2.commodity_id
+
+LEFT JOIN stations_v6 as buy_station_full on buy_station_id = buy_station_full.id
+LEFT JOIN stations_v6 as sell_station_full on sell_station_id = sell_station_full.id 
+LEFT JOIN systems_populated_v6 as buy_system_full on buy_system_full.id = buy_system_id
+LEFT JOIN systems_populated_v6 as sell_system_full on sell_system_full.id = sell_system_id
+
+
+where table1.station_id in
+
+	(select stations_v6.id as station_id from stations_v6 
+	where 	stations_v6.system_id = 12024
+	and 	stations_v6.max_landing_pad_size in ('L'))	
+
+and table1.supply != 0 
+and table1.buy_price != 0
+group by table1.commodity_id
+order by profit_per_unit desc
+
+
+
+LIMIT 50000
--- EDAR-server/index.ts
+++ EDAR-server/index.ts
@@ -4,22 +4,27 @@ import {refreshDatabase} from './database_tools/db_updaters';
 import {FindTradeOptions} from './database_tools/FindTradeOptions';
 import {findTrades, MIN_PAD_SIZE} from './database_tools/trade_finder';
 
+const refresh_db = false;
 
 export const db: Sqlite.Database = Sqlite('EDAR.sqlite3');
 // db.pragma('journal_mode = WAL');
 
-// refreshDatabase(true);
-console.timeEnd('loaded up in');
-console.time('Found a trade in');
-const tradeOpts = new FindTradeOptions('LHS 3447', 10, 100000, 16);
-tradeOpts.minPadSize = MIN_PAD_SIZE.L;
-let trade = findTrades(tradeOpts);
-// let returnTrade = undefined;
-// if(trade) {
-//   returnTrade = findReturnTrade(trade.buy_station_id, trade.)
-// }
-console.log(trade);
-tradeOpts.two_way = false;
-trade = findTrades(tradeOpts);
-console.log(trade);
-console.timeEnd('Found a trade in');
+if (refresh_db) {
+  refreshDatabase(true);
+} else {
+  console.timeEnd('loaded up in');
+  console.time('Found a trade in');
+  const tradeOpts = new FindTradeOptions('LHS 3447', 10, 100000, 16);
+  tradeOpts.minPadSize = MIN_PAD_SIZE.L;
+  let trade = findTrades(tradeOpts);
+
+  // let returnTrade = undefined;
+  // if(trade) {
+  //   returnTrade = findReturnTrade(trade.buy_station_id, trade.)
+  // }
+  console.log(trade);
+  tradeOpts.two_way = false;
+  trade = findTrades(tradeOpts);
+  console.log(trade);
+  console.timeEnd('Found a trade in');
+}
