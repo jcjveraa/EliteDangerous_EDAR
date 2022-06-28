59ac698720f060e9c4ab613dad5f100f22ecb960
JelleV <3942301+jcjveraa@users.noreply.github.com>
2022-06-28 14:53:37 +0000

    Typo corrected

--- EDAR-server/database_tools/revised_sql/trades_available.sql
+++ EDAR-server/database_tools/revised_sql/trades_available.sql
@@ -10,7 +10,7 @@ select
     @max_cargo_space
   ) as max_units,
   listings_in_current_system_XYZZYX.station_id as buy_from_station_id,
-  listings_in_current_system_XYZZYX.system_id as buy_fromsystem_id,
+  listings_in_current_system_XYZZYX.system_id as buy_from_system_id,
   listings_in_range_XYZZYX.station_id as sell_to_station_id,
   listings_in_range_XYZZYX.system_id as sell_to_system_id,
   listings_in_current_system_XYZZYX.in_system_travel_time + listings_in_range_XYZZYX.in_system_travel_time as total_travel_time
