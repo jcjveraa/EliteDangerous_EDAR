eb3b220aef8ea7ea24b7b7a193e2a6cfdc244027
JelleV <3942301+jcjveraa@users.noreply.github.com>
2022-07-02 15:51:48 +0200

    Some progress on the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,29 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import CssBaseline from '@mui/material/CssBaseline'
 import { Box, Typography } from '@mui/material';
 import { SystemNameSearchBox } from './SystemNameSearchBox';
 
 export default function App() {
+  const [selectedValue, setSelectedValue] = useState(undefined);
+
+  useEffect(() => {
+    if (selectedValue !== undefined) { console.log('Selected value changed!', selectedValue); }
+  }, [selectedValue]);
+
+  function clearSelectionHandler() {
+    console.log('Selection cleared!');
+    setSelectedValue(undefined);
+  }
+
   return (
     <React.Fragment>
       <CssBaseline />
       <Title />
-      <SystemNameSearchBox />
+      <SystemNameSearchBox
+        selectedValueHandler={setSelectedValue}
+        clearSelectionHandler={clearSelectionHandler}
+      />
     </React.Fragment>
   );
 }
--- client/src/SystemNameSearchBox.js
+++ client/src/SystemNameSearchBox.js
@@ -1,24 +1,43 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Autocomplete, TextField } from '@mui/material';
+import { useDebouncedCallback } from 'use-debounce';
 
 export function SystemNameSearchBox(props) {
   const [options, setOptions] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  // const [selectedValue, setSelectedValue] = useState([]);  
 
-  function handleInput(event) {
-    const currentValue = event.target.value;
-    setSearchTerm(currentValue);
-  }
+  const handleInput = useDebouncedCallback(
+    // function
+    (value) => {
+      getSystemNamesFromBackend(value);
+    },
+    // delay in ms
+    300
+  );
 
-  useEffect(() => {
+  function getSystemNamesFromBackend(searchTerm) {
     fetch('http://localhost:3001/api/SystemNameAutocomplete/' + searchTerm)
       .then(res => res.json())
       .then(data => setOptions(data));
-  }, [searchTerm]);
+  }
+
+  function onChangeHandler(event, value, reason) {
+    // console.log(reason);
+    if (reason === 'selectOption') {
+      props.selectedValueHandler(value);
+    }
+
+    if (reason === 'clear') {
+      props.clearSelectionHandler();
+    }
+
+  }
 
   return (
     <Autocomplete
       disablePortal
+      // autoComplete={true}
+      autoSelect={true}
       id="system-search-box"
       options={options}
       filterOptions={(x) => x}
@@ -26,9 +45,12 @@ export function SystemNameSearchBox(props) {
         p: 1, width: 300, alignContent: 'center',
         textAlign: 'left'
       }}
+      // onKeyUp={(e) => handleInput(e.target.value)} 
+      onInputChange={(e) => handleInput(e.target.value)}
+      onChange={(e, v, r) => onChangeHandler(e, v, r)}
       renderInput={(params) => <TextField
         {...params}
         label="Enter starting system name..."
-        onKeyUp={handleInput} />} />
+      />} />
   )
 }
